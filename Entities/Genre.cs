b08209e7e1a30f726e3a4da10d554fc4dcfb4c49
Eric Huang <86519473+erikku54@users.noreply.github.com>
2025-03-23 21:05:59 +0800

    feat: sec.27

---
+++ Entities/Genre.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace MinimalAPIsMovies.Entities;
+
+public class Genre
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using MinimalAPIsMovies.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+string authorName = builder.Configuration.GetValue<string>("AuthorName")!;
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
@@ -14,28 +19,49 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.MapGet("/", () => authorName);
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/genres", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+    var genres = new List<Genre>() {
+        new Genre { Id = 1, Name = "Drama" },
+        new Genre { Id = 2, Name = "Comedy" },
+        new Genre { Id = 3, Name = "Action" },
+        new Genre { Id = 4, Name = "Fantasy" },
+        new Genre { Id = 5, Name = "Horror" },
+        new Genre { Id = 6, Name = "Mystery" },
+        new Genre { Id = 7, Name = "Romance" },
+        new Genre { Id = 8, Name = "Thriller" },
+        new Genre { Id = 9, Name = "Western" }
+    };
+
+    return genres;
+});
+
+
+
+// var summaries = new[]
+// {
+//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+// };
+
+// app.MapGet("/weatherforecast", () =>
+// {
+//     var forecast =  Enumerable.Range(1, 5).Select(index =>
+//         new WeatherForecast
+//         (
+//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+//             Random.Shared.Next(-20, 55),
+//             summaries[Random.Shared.Next(summaries.Length)]
+//         ))
+//         .ToArray();
+//     return forecast;
+// })
+// .WithName("GetWeatherForecast");
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+// {
+//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+// }
