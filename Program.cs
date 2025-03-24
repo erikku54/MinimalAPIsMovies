d0a41be497095c588052cb789aeb6df6d614c43e
Eric Huang <86519473+erikku54@users.noreply.github.com>
2025-03-25 07:40:54 +0800

    feat: sec.38

--- Program.cs
+++ Program.cs
@@ -3,15 +3,35 @@ using MinimalAPIsMovies.Entities;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
 string authorName = builder.Configuration.GetValue<string>("AuthorName")!;
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(corsPolicyBuilder =>
+    {
+        corsPolicyBuilder.WithOrigins(builder.Configuration["AllowedOrigins"]!.Split(","))
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+    options.AddPolicy("free", corsPo =>
+    {
+        corsPo.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddOutputCache();
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors("free");
+app.UseOutputCache();
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
@@ -36,32 +56,7 @@ app.MapGet("/genres", () =>
     };
 
     return genres;
-});
-
-
-
-// var summaries = new[]
-// {
-//     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-// };
-
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast =  Enumerable.Range(1, 5).Select(index =>
-//         new WeatherForecast
-//         (
-//             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//             Random.Shared.Next(-20, 55),
-//             summaries[Random.Shared.Next(summaries.Length)]
-//         ))
-//         .ToArray();
-//     return forecast;
-// })
-// .WithName("GetWeatherForecast");
+}).CacheOutput(c => c.Expire(TimeSpan.FromSeconds(15)));
 
 app.Run();
 
-// record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-// {
-//     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-// }
