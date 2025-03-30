f77dfd3f177f069ed0c0ea7ff9e9ab9ab19df08c
Eric Huang <86519473+erikku54@users.noreply.github.com>
2025-03-30 22:30:10 +0800

    feat: sec.69

--- Endpoints/ActorEndpoints.cs
+++ Endpoints/ActorEndpoints.cs
@@ -15,7 +15,9 @@ public static class ActorEndpoints
     private readonly static string _container = "actors";
     public static RouteGroupBuilder MapActors(this RouteGroupBuilder builder)
     {
-        builder.MapPost("/create", Create).DisableAntiforgery();
+        builder.MapPost("/", Create).DisableAntiforgery();
+        builder.MapGet("/", GetAll).CacheOutput(c => c.Expire(TimeSpan.FromSeconds(60)).Tag("actors-get"));
+        builder.MapGet("/{id:int}", GetById);
 
 
         return builder;
@@ -37,4 +39,22 @@ public static class ActorEndpoints
         return TypedResults.Created($"/actors/{id}", actorDTO);
     }
 
+    static async Task<Ok<List<ActorDTO>>> GetAll(IActorsRepository actorsRepository, IMapper mapper)
+    {
+        var actors = await actorsRepository.GetAll();
+        var actorsDTO = mapper.Map<List<ActorDTO>>(actors);
+        return TypedResults.Ok(actorsDTO);
+    }
+
+    static async Task<Results<Ok<ActorDTO>, NotFound>> GetById(int id, IActorsRepository actorsRepository, IMapper mapper)
+    {
+        var actor = await actorsRepository.GetById(id);
+        if (actor is null)
+        {
+            return TypedResults.NotFound();
+        }
+
+        var actorDTO = mapper.Map<ActorDTO>(actor);
+        return TypedResults.Ok(actorDTO);
+    }
 }
